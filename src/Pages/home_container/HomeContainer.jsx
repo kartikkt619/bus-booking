ac66bc1fd041cf88efaf1983f30cfbf31108512a
Kartikkt619 <thakurkartik619@gmail.com>
2025-04-09 17:00:58 +0530

    bus booking reactjs

---
+++ src/Pages/home_container/HomeContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './hero/Hero'
+
+const HomeContainer = () => {
+  return (
+    <div>
+         {/* Hero section and other home related content */}
+
+         <Hero />
+    </div>
+  )
+}
+
+export default HomeContainer
